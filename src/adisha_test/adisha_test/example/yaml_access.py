39ec58c30c25cebd27d3953ab0f1b393262962bc
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2023-09-17 00:31:58 +0700

    Feat(adisha_test): yaml_access.py example for dumping new .yaml file added

--- src/adisha_test/adisha_test/example/yaml_access.py
+++ src/adisha_test/adisha_test/example/yaml_access.py
@@ -13,6 +13,13 @@ def main(args=None):
     # Print a value
     print(test_yaml['profil_robot']['tinggi'])
 
+    # Edit the file
+    test_yaml['versi'] = '0.0.0'
+
+    # Overwrite
+    with open(TEST_YAML_PATH, 'w') as file:
+        yaml.safe_dump(test_yaml, file)
+
 
 
 if __name__ == '__main__':
