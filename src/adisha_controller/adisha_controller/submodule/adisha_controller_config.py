eea60a51b802e50f9a9819d9351bd29a97bc5a3b
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2023-09-28 12:23:41 +0700

    Feat(adisha_controller): adisha_controller_config.py are changed to ease the config and adisha_dxlsyncrwpos_node.py are completed and built successfully

--- src/adisha_controller/adisha_controller/submodule/adisha_controller_config.py
+++ src/adisha_controller/adisha_controller/submodule/adisha_controller_config.py
@@ -1,19 +1,35 @@
 import yaml
 
+
 ADISHA_ROBOT_INFO_YAML_PATH = 'src/adisha_main/config/adisha_robot_info.yaml'
 ADISHA_JOINT_INFO_YAML_PATH = 'src/adisha_controller/config/adisha_joints_info.yaml'
 ADISHA_POSE_YAML_PATH       = 'src/adisha_controller/config/adisha_pose.yaml'
 ADISHA_MOTION_SEQUENCE_PATH = 'src/adisha_controller/config/motion_sequence/'
 
+
 ADISHA_ROBOT_INFO_YAML      = yaml.safe_load(open(ADISHA_ROBOT_INFO_YAML_PATH, 'r'))
 ADISHA_JOINT_INFO_YAML      = yaml.safe_load(open(ADISHA_JOINT_INFO_YAML_PATH, 'r'))
 ADISHA_POSE_YAML            = yaml.safe_load(open(ADISHA_POSE_YAML_PATH, 'r'))
 
-ROBOT_NAME                  = ADISHA_ROBOT_INFO_YAML['name']
-JOINT_NUM                   = ADISHA_JOINT_INFO_YAML['joint_num']
-JOINT_LIST                  = ADISHA_JOINT_INFO_YAML['joint_list']
-JOINT_ID                    = ADISHA_JOINT_INFO_YAML['joint_id']
-JOINT_SERVO                 = ADISHA_JOINT_INFO_YAML['joint_servo']
+
+ADISHA_ROBOT_NAME           = ADISHA_ROBOT_INFO_YAML['name']
+ADISHA_JOINT_NUM            = ADISHA_JOINT_INFO_YAML['joint_num']
+ADISHA_JOINT_LIST           = ADISHA_JOINT_INFO_YAML['joint_list']
+ADISHA_JOINT_ID             = ADISHA_JOINT_INFO_YAML['joint_id']
+ADISHA_JOINT_SERVO          = []
+ADISHA_DXL_ERROR            = 99999
+
+ADISHA_XL320    = 0
+ADISHA_AX12A    = 1
+ADISHA_MX28     = 2
+for servo_name in ADISHA_JOINT_INFO_YAML['joint_servo']:
+    if servo_name == 'XL320':
+        ADISHA_JOINT_SERVO.append(ADISHA_XL320)
+    elif servo_name == 'AX12A':
+        ADISHA_JOINT_SERVO.append(ADISHA_AX12A)
+    elif servo_name == 'MX28':
+        ADISHA_JOINT_SERVO.append(ADISHA_MX28)
+
 
 XL320_ADDR_TORQUE_ENABLE    = ADISHA_JOINT_INFO_YAML['XL320']['addr_torque_enable']
 AX12A_ADDR_TORQUE_ENABLE    = ADISHA_JOINT_INFO_YAML['AX12A']['addr_torque_enable']
--- src/adisha_controller/adisha_controller/submodule/adisha_dxlsyncrwpos_node.py
+++ src/adisha_controller/adisha_controller/submodule/adisha_dxlsyncrwpos_node.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from .adisha_controller_config import *
-import adisha_interface.msg as adisha_msg
+import adisha_interface.msg as adisha_msgs
 import dynamixel_sdk as dxlsdk
 
 
@@ -11,93 +11,177 @@ class AdishaDxlSyncRWPos(Node):
 
 
     def __init__(self) -> None:
-        super().__init__(f'{ROBOT_NAME}_DXLSyncRWPos')
+        super().__init__(f'{ADISHA_ROBOT_NAME}_DXLSyncRWPos')
 
         
         # Constants
-        self.SPIN_RATE      = 1./30.
         self.USB_DEVICE     = '/dev/ttyUSB0'
         self.DXL_PROTOCOL_1 = 1.0
         self.DXL_PROTOCOL_2 = 2.0
         self.DXL_BAUDRATE   = 1000000
+        self.TIMER1_RATE    = 1./30.
 
         
-        # Create ROS2 instances
-        self.present_pos_pub    = self.create_publisher(adisha_msg.JointVal, f'/{ROBOT_NAME}/joint/present_pos', 10)
-        self.torque_enable_sub  = self.create_subscription(adisha_msg.JointTorque, f'/{ROBOT_NAME}/joint/torque_enable', self.cbJointTorqueEnable, 10)
-        self.goal_pos_sub       = self.create_subscription(adisha_msg.JointVal, f'/{ROBOT_NAME}/joint/goal_pos', self.cbJointGoalPos, 10)
-        self.timer_routine      = self.create_timer(self.SPIN_RATE, self.timerRoutine)
+        # Create ROS2 publishers and subscribers
+        self.present_pos_pub    = self.create_publisher(adisha_msgs.JointVal, f'/{ADISHA_ROBOT_NAME}/joint/present_pos', 10)
+        self.torque_enable_sub  = self.create_subscription(adisha_msgs.JointTorque, f'/{ADISHA_ROBOT_NAME}/joint/torque_enable', self.cbJointTorqueEnable, 10)
+        self.goal_pos_sub       = self.create_subscription(adisha_msgs.JointVal, f'/{ADISHA_ROBOT_NAME}/joint/goal_pos', self.cbJointGoalPos, 10)
+        
 
-        # self.test_pub           = self.create_publisher(adisha_msg.JointTorque, f'/{ROBOT_NAME}/joint/torque_enable', 10)
-        # self.test_msg           = adisha_msg.JointTorque()
-        # self.test_msg.head      = True
-        # self.test_msg.neck      = True
-        # self.test_msg.l_sho_r   = True
-        # self.test_msg.r_sho_r   = True
-        # self.test_msg.l_sho_p   = True
-        # self.test_msg.r_sho_p   = True
-        # self.test_msg.l_arm_y   = True
-        # self.test_msg.r_arm_y   = True
-        # self.test_msg.l_arm_p   = True
-        # self.test_msg.r_arm_p   = True
-        # self.test_msg.l_wrist   = True
-        # self.test_msg.r_wrist   = True
-        # self.test_msg.l_hip_y   = True
-        # self.test_msg.r_hip_y   = True 
-        # self.test_msg.l_hip_r   = True  
-        # self.test_msg.r_hip_r   = True  
-        # self.test_msg.l_hip_p   = True  
-        # self.test_msg.r_hip_p   = True  
-        # self.test_msg.l_knee    = True
-        # self.test_msg.r_knee    = True
-        # self.test_msg.l_ank_p   = True
-        # self.test_msg.r_ank_p   = True
-        # self.test_msg.l_ank_r   = True
-        # self.test_msg.r_ank_r   = True
+        # Create ROS2 timers
+        # timer1 used for publishing present position
+        self.timer1_routine     = self.create_timer(self.TIMER1_RATE, self.timer1Routine)
+
+
+        # Messages
+        self.present_pos_msg    = adisha_msgs.JointVal()
 
 
         # Dynamixel SDK interfaces
-        self.dxl_port_handler       = dxlsdk.PortHandler(self.USB_DEVICE)   
         self.dxl_packet_handler1    = dxlsdk.PacketHandler(self.DXL_PROTOCOL_1)
         self.dxl_packet_handler2    = dxlsdk.PacketHandler(self.DXL_PROTOCOL_2)
-        self.dxl_port_handler.openPort()
-        self.dxl_port_handler.setBaudRate(self.DXL_BAUDRATE)
+        self.dxl_port_handler       = dxlsdk.PortHandler(self.USB_DEVICE)
+        
+        if self.dxl_port_handler.openPort():
+            self.get_logger().info(f'Succeeded to open {self.USB_DEVICE} port!')
+        else:
+            self.get_logger().error(f'Failed to open {self.USB_DEVICE} port!')
+            self.get_logger().error('Node destroyed.')
+            self.destroy_node()
+            exit()
 
+        if self.dxl_port_handler.setBaudRate(self.DXL_BAUDRATE):
+            self.get_logger().info(f'Succeeded to change the baudrate to {self.DXL_BAUDRATE}!')
+        else:
+            self.get_logger().error(f'Failed to change the baudrate to {self.DXL_BAUDRATE}!')
+            self.get_logger().error('Node destroyed.')
+            self.destroy_node()
+            exit()
 
+        # Note: in Dynamixel protocol 1.0, both AX-12A and MX-28 have the same address and data size
         self.gsw2_arm_torque_enable     = dxlsdk.GroupSyncWrite(self.dxl_port_handler, self.dxl_packet_handler2, XL320_ADDR_TORQUE_ENABLE, XL320_SIZE_TORQUE_ENABLE)
-        self.gsw1_uleg_torque_enable    = dxlsdk.GroupSyncWrite(self.dxl_port_handler, self.dxl_packet_handler1, AX12A_ADDR_TORQUE_ENABLE, AX12A_SIZE_TORQUE_ENABLE)
-        self.gsw1_lleg_torque_enable    = dxlsdk.GroupSyncWrite(self.dxl_port_handler, self.dxl_packet_handler1, MX28_ADDR_TORQUE_ENABLE, MX28_SIZE_TORQUE_ENABLE)
+        self.gsw1_leg_torque_enable     = dxlsdk.GroupSyncWrite(self.dxl_port_handler, self.dxl_packet_handler1, MX28_ADDR_TORQUE_ENABLE, MX28_SIZE_TORQUE_ENABLE)
 
         self.gsw2_arm_goal_pos          = dxlsdk.GroupSyncWrite(self.dxl_port_handler, self.dxl_packet_handler2, XL320_ADDR_GOAL_POS, XL320_SIZE_GOAL_POS)
-        self.gsw1_uleg_goal_pos         = dxlsdk.GroupSyncWrite(self.dxl_port_handler, self.dxl_packet_handler1, AX12A_ADDR_GOAL_POS, AX12A_SIZE_GOAL_POS)
-        self.gsw1_lleg_goal_pos         = dxlsdk.GroupSyncWrite(self.dxl_port_handler, self.dxl_packet_handler1, MX28_ADDR_GOAL_POS, MX28_SIZE_GOAL_POS)
+        self.gsw1_leg_goal_pos          = dxlsdk.GroupSyncWrite(self.dxl_port_handler, self.dxl_packet_handler1, MX28_ADDR_GOAL_POS, MX28_SIZE_GOAL_POS)
 
         self.gsr2_arm_present_pos       = dxlsdk.GroupSyncRead(self.dxl_port_handler, self.dxl_packet_handler2, XL320_ADDR_PRESENT_POS, XL320_SIZE_PRESENT_POS)
         self.gbr1_leg_present_pos       = dxlsdk.GroupBulkRead(self.dxl_port_handler, self.dxl_packet_handler1)
 
+        # Loop through all group sync read and group bulk read to assign ID's
+        for idx in range(ADISHA_JOINT_NUM):
+
+            if ADISHA_JOINT_SERVO[idx] == ADISHA_XL320:
+                self.gsr2_arm_present_pos.addParam(ADISHA_JOINT_ID[idx])
+
+            elif ADISHA_JOINT_SERVO[idx] == ADISHA_AX12A or ADISHA_JOINT_SERVO[idx] == ADISHA_MX28:
+                self.gbr1_leg_present_pos.addParam(ADISHA_JOINT_ID[idx], MX28_ADDR_PRESENT_POS, MX28_SIZE_PRESENT_POS)
+
 
     
     def setTorqueEnable(self, torque_values:list) -> None:
-        for idx in range(JOINT_NUM):
-            param = [dxlsdk.DXL_LOBYTE(dxlsdk.DXL_LOWORD(torque_values[idx]))]
-            if JOINT_SERVO[idx] == 'XL320':
-                self.gsw2_arm_torque_enable.addParam(JOINT_ID[idx], param)
+        for idx in range(ADISHA_JOINT_NUM):
+            # Torque enable is a 1-byte parameter
+            param = [dxlsdk.DXL_LOBYTE(torque_values[idx])]
 
-            elif JOINT_SERVO[idx] == 'AX12A':
-                self.gsw1_uleg_torque_enable.addParam(JOINT_ID[idx], param)
+            if ADISHA_JOINT_SERVO[idx] == ADISHA_XL320:
+                dxl_result = self.gsw2_arm_torque_enable.addParam(ADISHA_JOINT_ID[idx], param)
+                if not dxl_result:
+                    self.get_logger().error(f'Torque enable addparam failed [ID:{ADISHA_JOINT_ID[idx]}, NAME:{ADISHA_JOINT_LIST[idx]}]')
 
-            if JOINT_SERVO[idx] == 'MX28':    
-                self.gsw1_lleg_torque_enable.addParam(JOINT_ID[idx], param)
+            elif ADISHA_JOINT_SERVO[idx] == ADISHA_AX12A or ADISHA_JOINT_SERVO[idx] == ADISHA_MX28:
+                dxl_result = self.gsw1_leg_torque_enable.addParam(ADISHA_JOINT_ID[idx], param)
+                if not dxl_result:
+                    self.get_logger().error(f'Torque enable addparam failed [ID:{ADISHA_JOINT_ID[idx]}, NAME:{ADISHA_JOINT_LIST[idx]}]')
         
+
         # Send packet
-        self.gsw2_arm_torque_enable.txPacket()
-        self.gsw1_uleg_torque_enable.txPacket()
-        self.gsw1_lleg_torque_enable.txPacket()
+        dxl_result = self.gsw2_arm_torque_enable.txPacket()
+        if dxl_result != dxlsdk.COMM_SUCCESS:
+            self.get_logger().error('Arm torque enable txPacket failed!')
+            self.get_logger().error(f'{self.dxl_packet_handler2.getTxRxResult(dxl_result)}')
+
+        dxl_result = self.gsw1_leg_torque_enable.txPacket()
+        if dxl_result != dxlsdk.COMM_SUCCESS:
+            self.get_logger().error('Leg torque enable txPacket failed!')
+            self.get_logger().error(f'{self.dxl_packet_handler1.getTxRxResult(dxl_result)}')
+
 
         # Clear group sync write buffer
         self.gsw2_arm_torque_enable.clearParam()
-        self.gsw1_uleg_torque_enable.clearParam()
-        self.gsw1_lleg_torque_enable.clearParam()
+        self.gsw1_leg_torque_enable.clearParam()
+
+
+
+    def setGoalPos(self, goal_pos:list) -> None:
+        for idx in range(ADISHA_JOINT_NUM):
+            # All of the servo's in our particular case have the same size of goal position, 2-bytes
+            param = [dxlsdk.DXL_LOBYTE(goal_pos[idx]), dxlsdk.DXL_HIBYTE(goal_pos[idx])]
+
+            if ADISHA_JOINT_SERVO[idx] == ADISHA_XL320:
+                dxl_result = self.gsw2_arm_goal_pos.addParam(ADISHA_JOINT_ID[idx], param)
+                if not dxl_result:
+                    self.get_logger().error(f'Goal pos addparam failed [ID:{ADISHA_JOINT_ID[idx]}, NAME:{ADISHA_JOINT_LIST[idx]}]')
+
+            elif ADISHA_JOINT_SERVO[idx] == ADISHA_AX12A or ADISHA_JOINT_SERVO[idx] == ADISHA_MX28:
+                dxl_result = self.gsw1_leg_goal_pos.addParam(ADISHA_JOINT_ID[idx], param)
+                if not dxl_result:
+                    self.get_logger().error(f'Goal pos addparam failed [ID:{ADISHA_JOINT_ID[idx]}, NAME:{ADISHA_JOINT_LIST[idx]}]')
+
+
+        # Send packet
+        dxl_result = self.gsw2_arm_goal_pos.txPacket()
+        if dxl_result != dxlsdk.COMM_SUCCESS:
+            self.get_logger().error('Arm goal pos txPacket failed!')
+            self.get_logger().error(f'{self.dxl_packet_handler2.getTxRxResult(dxl_result)}')
+
+        dxl_result = self.gsw1_leg_goal_pos.txPacket()
+        if dxl_result != dxlsdk.COMM_SUCCESS:
+            self.get_logger().error('Leg goal pos txPacket failed!')
+            self.get_logger().error(f'{self.dxl_packet_handler1.getTxRxResult(dxl_result)}')
+
+
+        # Clear group sync write buffer
+        self.gsw2_arm_goal_pos.clearParam()
+        self.gsw1_leg_goal_pos.clearParam()
+
+
+
+    def getPresentPos(self) -> list:
+        # Get packets
+        dxl_result = self.gsr2_arm_present_pos.txRxPacket()
+        if dxl_result != dxlsdk.COMM_SUCCESS:
+            self.get_logger().error('Arm present pos txRxPacket failed!')
+            self.get_logger().error(f'{self.dxl_packet_handler2.getTxRxResult(dxl_result)}')
+
+        dxl_result = self.gbr1_leg_present_pos.txRxPacket()
+        if dxl_result != dxlsdk.COMM_SUCCESS:
+            self.get_logger().error('Arm present pos txRxPacket failed!')
+            self.get_logger().error(f'{self.dxl_packet_handler1.getTxRxResult(dxl_result)}')
+
+
+        # Loop through all joints
+        present_pos_data = []
+        for idx in range(ADISHA_JOINT_NUM):
+            if ADISHA_JOINT_SERVO[idx] == ADISHA_XL320:
+                dxl_result = self.gsr2_arm_present_pos.isAvailable(ADISHA_JOINT_ID[idx], XL320_ADDR_PRESENT_POS, XL320_SIZE_PRESENT_POS)
+                if not dxl_result:
+                    self.get_logger().error(f'Present pos getData failed [ID:{ADISHA_JOINT_ID[idx]}, NAME:{ADISHA_JOINT_LIST[idx]}]')
+                    present_pos_data.append(ADISHA_DXL_ERROR)
+                else:
+                    present_pos_data.append(self.gsr2_arm_present_pos.getData(ADISHA_JOINT_ID[idx], XL320_ADDR_PRESENT_POS, XL320_SIZE_PRESENT_POS))
+
+            elif ADISHA_JOINT_SERVO[idx] == ADISHA_AX12A or ADISHA_JOINT_SERVO[idx] == ADISHA_MX28:
+                dxl_result = self.gbr1_leg_present_pos.isAvailable(ADISHA_JOINT_ID[idx], MX28_ADDR_PRESENT_POS, MX28_SIZE_PRESENT_POS)
+                if not dxl_result:
+                    self.get_logger().error(f'Present pos getData failed [ID:{ADISHA_JOINT_ID[idx]}, NAME:{ADISHA_JOINT_LIST[idx]}]')
+                    present_pos_data.append(ADISHA_DXL_ERROR)
+                else:
+                    present_pos_data.append(self.gbr1_leg_present_pos.getData(ADISHA_JOINT_ID[idx], MX28_ADDR_PRESENT_POS, MX28_SIZE_PRESENT_POS))
+        
+
+        # Return the data for publishing
+        return present_pos_data
 
 
 
@@ -128,39 +212,68 @@ class AdishaDxlSyncRWPos(Node):
             msg.l_ank_r,
             msg.r_ank_r
         ]
-        self.setTorqueEnable(torque_values)
+        self.setTorqueEnable(torque_values=torque_values)
 
 
 
     def cbJointGoalPos(self, msg) -> None:
-        pass
+        goal_pos = [
+            msg.head,
+            msg.neck,
+            msg.l_sho_r,
+            msg.r_sho_r,
+            msg.l_sho_p,
+            msg.r_sho_p,
+            msg.l_arm_y,
+            msg.r_arm_y,
+            msg.l_arm_p,
+            msg.r_arm_p,
+            msg.l_wrist,
+            msg.r_wrist,
+            msg.l_hip_y,
+            msg.r_hip_y,
+            msg.l_hip_r,
+            msg.r_hip_r,
+            msg.l_hip_p,
+            msg.r_hip_p,
+            msg.l_knee,
+            msg.r_knee,
+            msg.l_ank_p,
+            msg.r_ank_p,
+            msg.l_ank_r,
+            msg.r_ank_r
+        ]
+        self.setGoalPos(goal_pos=goal_pos)
 
 
 
-    def timerRoutine(self) -> None:
-        pass
-        # self.test_pub.publish(self.test_msg)
-        # self.test_msg.head      = not self.test_msg.head
-        # self.test_msg.neck      = not self.test_msg.neck   
-        # self.test_msg.l_sho_r   = not self.test_msg.l_sho_r
-        # self.test_msg.r_sho_r   = not self.test_msg.r_sho_r
-        # self.test_msg.l_sho_p   = not self.test_msg.l_sho_p
-        # self.test_msg.r_sho_p   = not self.test_msg.r_sho_p
-        # self.test_msg.l_arm_y   = not self.test_msg.l_arm_y
-        # self.test_msg.r_arm_y   = not self.test_msg.r_arm_y
-        # self.test_msg.l_arm_p   = not self.test_msg.l_arm_p
-        # self.test_msg.r_arm_p   = not self.test_msg.r_arm_p
-        # self.test_msg.l_wrist   = not self.test_msg.l_wrist
-        # self.test_msg.r_wrist   = not self.test_msg.r_wrist
-        # self.test_msg.l_hip_y   = not self.test_msg.l_hip_y
-        # self.test_msg.r_hip_y   = not self.test_msg.r_hip_y 
-        # self.test_msg.l_hip_r   = not self.test_msg.l_hip_r  
-        # self.test_msg.r_hip_r   = not self.test_msg.r_hip_r  
-        # self.test_msg.l_hip_p   = not self.test_msg.l_hip_p  
-        # self.test_msg.r_hip_p   = not self.test_msg.r_hip_p 
-        # self.test_msg.l_knee    = not self.test_msg.l_knee 
-        # self.test_msg.r_knee    = not self.test_msg.r_knee
-        # self.test_msg.l_ank_p   = not self.test_msg.l_ank_p
-        # self.test_msg.r_ank_p   = not self.test_msg.r_ank_p
-        # self.test_msg.l_ank_r   = not self.test_msg.l_ank_r 
-        # self.test_msg.r_ank_r   = not self.test_msg.r_ank_r
+    def timer1Routine(self) -> None:
+        # Assign values
+        present_post_data = self.getPresentPos()
+        self.present_pos_msg.head       = present_post_data[0]
+        self.present_pos_msg.neck       = present_post_data[1]
+        self.present_pos_msg.l_sho_r    = present_post_data[2]
+        self.present_pos_msg.r_sho_r    = present_post_data[3]
+        self.present_pos_msg.l_sho_p    = present_post_data[4]
+        self.present_pos_msg.r_sho_p    = present_post_data[5]
+        self.present_pos_msg.l_arm_y    = present_post_data[6]
+        self.present_pos_msg.r_arm_y    = present_post_data[7]
+        self.present_pos_msg.l_arm_p    = present_post_data[8]
+        self.present_pos_msg.r_arm_p    = present_post_data[9]
+        self.present_pos_msg.l_wrist    = present_post_data[10]
+        self.present_pos_msg.r_wrist    = present_post_data[11]
+        self.present_pos_msg.l_hip_y    = present_post_data[12]
+        self.present_pos_msg.r_hip_y    = present_post_data[13]
+        self.present_pos_msg.l_hip_r    = present_post_data[14]
+        self.present_pos_msg.r_hip_r    = present_post_data[15]
+        self.present_pos_msg.l_hip_p    = present_post_data[16]
+        self.present_pos_msg.r_hip_p    = present_post_data[17]
+        self.present_pos_msg.l_knee     = present_post_data[18]
+        self.present_pos_msg.r_knee     = present_post_data[19]
+        self.present_pos_msg.l_ank_p    = present_post_data[20]
+        self.present_pos_msg.r_ank_p    = present_post_data[21]
+        self.present_pos_msg.l_ank_r    = present_post_data[22]
+        self.present_pos_msg.r_ank_r    = present_post_data[23]
+
+        # Publish
+        self.present_pos_pub.publish(self.present_pos_msg)
